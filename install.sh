1c17c48297d8942e997a1b110a175e56d97e9ae7
potassiummmm <54800242+potassiummmm@users.noreply.github.com>
2021-01-11 11:11:58 +0800

    Add plugins installation script

--- install.sh
+++ install.sh
@@ -5,9 +5,12 @@ if !( which zsh ); then
 fi
 
 if !( $ZSH_THEME ); then
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo Y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 cp ./.zshrc ~/
 
 cp ./myagnosterlight.zsh-theme ~/.oh-my-zsh/themes/ 
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+source ~/.zshrc
